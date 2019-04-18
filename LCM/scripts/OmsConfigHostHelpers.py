bd1e3f8faa28424a07dad5537c97eec51276a132
Zia Jalali <zijalali@microsoft.com>
2019-04-18 16:10:01 -0700

    fix telemetry format string for old python versions

--- LCM/scripts/OmsConfigHostHelpers.py
+++ LCM/scripts/OmsConfigHostHelpers.py
@@ -22,7 +22,7 @@ def write_omsconfig_host_telemetry(message):
 
     host_telemetry_json['message'] += message
 
-    with open(dsc_host_telemetry_path, 'a+') as host_telemetry_file:
+    with open(dsc_host_telemetry_path, 'w+') as host_telemetry_file:
         json.dump(host_telemetry_json, host_telemetry_file)
 
 def write_omsconfig_host_event(pathToCurrentScript, dsc_host_switch_exists):
@@ -32,5 +32,24 @@ def write_omsconfig_host_event(pathToCurrentScript, dsc_host_switch_exists):
         msg_buffer = 'Using dsc_host'
     else:
         msg_buffer = 'Falling back to OMI'
-    message = msg_template.format(timestamp, os.getpid(), 'INFO', 0, pathToCurrentScript, 0, msg_buffer)
+    message = msg_template % (timestamp, os.getpid(), 'INFO', 0, pathToCurrentScript, 0, msg_buffer)
     write_omsconfig_host_telemetry(message)
+
+def write_omsconfig_host_log(pathToCurrentScript, message, level = 'INFO'):
+    log_entry_template = '<OMSCONFIGLOG>[%s] [%d] [%s] [%d] [%s:%d] %s</OMSCONFIGLOG>'
+    timestamp = datetime.datetime.fromtimestamp(time.time()).strftime('%Y/%m/%d %H:%M:%S')
+    log_entry = log_entry_template % (timestamp, os.getpid(), level, 0, pathToCurrentScript, 0, message)
+
+    omsconfig_log_folder = '/var/opt/microsoft/omsconfig'
+    if not os.path.exists(omsconfig_log_folder):
+        os.makedirs(omsconfig_log_folder)
+
+    print(log_entry)
+
+    omsconfig_log_path = os.path.join(omsconfig_log_folder, 'omsconfig.log')
+    with open(omsconfig_log_path, 'a+') as omsconfig_log_file:
+        omsconfig_log_file.write(log_entry)
+
+    omsconfig_detailed_log_path = os.path.join(omsconfig_log_folder, 'omsconfigdetailed.log')
+    with open(omsconfig_detailed_log_path, 'a+') as omsconfig_detailed_log_file:
+        omsconfig_detailed_log_file.write(log_entry)
